0841225f0a8cea3d2ed75c32595b693e00677afc
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2024-09-16 19:27:28 +0530

    Add functionality to the education section
    Somehow missed it before.

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -2,14 +2,43 @@ import Input from '../../utils/Input.jsx';
 
 function Education({ content, setContent }) {
   const { school, degree, startDate, endDate, location } = content;
+  const setValue = (valueObj) => {
+    setContent({ ...content, ...valueObj });
+  };
+
   return (
     <div className="card">
       <h3 className="card__title">Education</h3>
-      <Input id="school" label="School" value={school} />
-      <Input id="degree" label="Degree" value={degree} />
-      <Input id="start-date" label="Start Date" value={startDate} />
-      <Input id="end-date" label="End Date" value={endDate} />
-      <Input id="location" label="Location" value={location} />
+      <Input
+        id="school"
+        label="School"
+        value={school}
+        onChange={(e) => setValue({ school: e.target.value })}
+      />
+      <Input
+        id="degree"
+        label="Degree"
+        value={degree}
+        onChange={(e) => setValue({ degree: e.target.value })}
+      />
+      <Input
+        id="start-date"
+        label="Start Date"
+        value={startDate}
+        onChange={(e) => setValue({ startDate: e.target.value })}
+      />
+      <Input
+        id="end-date"
+        label="End Date"
+        value={endDate}
+        onChange={(e) => setValue({ endDate: e.target.value })}
+      />
+      <Input
+        id="location"
+        label="Location"
+        value={location}
+        onChange={(e) => setValue({ location: e.target.value })}
+      />
     </div>
   );
 }
